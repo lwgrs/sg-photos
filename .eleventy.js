683be94adbbd6baebdfab0bab0feff24d2ffa27d
lwgrs <srgower@gmail.com>
2024-10-22 18:23:41 -0400

    Made additions to frontmatter options and base.njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,19 @@
 module.exports = function (eleventyConfig) {
+  let options = {
+    html: true, // Enable HTML tags in source
+    breaks: true,  // Convert '\n' in paragraphs into <br>
+    linkify: true // Autoconvert URL-like text to links
+  };
+
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/img");
-    eleventyConfig.addPassthroughCopy("./src/fonts");
-    eleventyConfig.addPassthroughCopy("./src/js");
 
+    eleventyConfig.addFilter("readableDate", dateObj => {
+      return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    });
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+      return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    });
   
     return {
         markdownTemplateEngine: "njk",
