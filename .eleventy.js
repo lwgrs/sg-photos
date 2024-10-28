bc249acf7f703b4a44f771b39df2064893c0fb7b
lwgrs <137942185+lwgrs@users.noreply.github.com>
2024-10-28 10:22:41 -0400

    The "getFullYear" function was missing from eleventy.js - something fishy there.

--- .eleventy.js
+++ .eleventy.js
@@ -20,6 +20,11 @@ eleventyConfig.addFilter('htmlDateString', (dateObj) => {
   return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
 });
 
+eleventyConfig.addFilter("getFullYear", (dateString) => {
+  dateObj = new Date(dateString);
+  return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy');
+});
+
 eleventyConfig.addPlugin(pluginRss, {
   posthtmlRenderOptions: {
     closingSingleTag: "default" // opt-out of <img/>-style XHTML single tags 
